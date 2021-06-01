27c3a1ecdd5dd92bf22626f8697593ee19847e96
Rock Baek <rbaek@splunk.com>
2021-06-01 16:22:41 -0700

    fix typo

--- .circleci/deploy_connector.sh
+++ .circleci/deploy_connector.sh
@@ -15,7 +15,7 @@ helm install ci-sck --set image.repository=$OTEL_CONTRIB_IMAGE \
 --set splunk_hec.index=$CI_INDEX_EVENTS \
 --set splunk_hec.token=$CI_SPLUNK_HEC_TOKEN \
 --set splunk_hec.endpoint=https://$CI_SPLUNK_HOST:8088/services/collector \
---set containers.containerRunTime=$CONTAINER_RUNTIME \
+--set containers.containerRuntime=$CONTAINER_RUNTIME \
 -f .circleci/sck_otel_values.yaml charts/opentelemetry-collector/
 #wait for deployment to finish
 until kubectl get pod | grep Running | [[ $(wc -l) == 1 ]]; do
