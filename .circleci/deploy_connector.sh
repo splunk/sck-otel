e473996ef6db33071d9e45e9d22f2cb25f39bca1
Mei <meiw@splunk.com>
2021-06-11 09:26:21 -0700

    Create build_test_sck_otel-docker.yaml

--- .circleci/deploy_connector.sh
+++ .circleci/deploy_connector.sh
@@ -10,9 +10,7 @@ else
    helm delete $(helm ls --short)
 fi
 echo "Deploying sck-otel-connect with latest changes"
-helm install ci-sck --set image.repository=$OTEL_CONTRIB_IMAGE \
---set image.tag=$OTEL_CONTRIB_IMAGE_TAG \
---set splunk_hec.index=$CI_INDEX_EVENTS \
+helm install ci-sck --set splunk_hec.index=$CI_INDEX_EVENTS \
 --set splunk_hec.token=$CI_SPLUNK_HEC_TOKEN \
 --set splunk_hec.endpoint=https://$CI_SPLUNK_HOST:8088/services/collector \
 --set containers.containerRuntime=$CONTAINER_RUNTIME \
